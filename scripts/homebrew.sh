ba5bd38f9c4708592913cb765a36c992ce33deb1
Zac Robinson <zacrobinson1992@gmail.com>
2024-11-27 15:25:26 +0000

    Update homebrew.sh

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -3,7 +3,7 @@ echo "Installing homebrew ..."
 
 which -s brew
 if [[ $? != 0 ]] ; then
-  yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  yes |/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew already installed ..."
 fi
